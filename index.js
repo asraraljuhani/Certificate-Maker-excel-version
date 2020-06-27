c63c60a141522435d3efc68af498e075c5f42b63
AsrarAljuhani <asrarbaljuhani@gmail.com>
2020-06-27 14:09:53 +0300

    fix

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 const path = require('path')
-// app.use(express.static(path.join(__dirname, 'public')))
-app.get('/', function(req, res) {
-  // res.sendFile(path.join(__dirname + '/public'));
-  // app.use(express.static(path.join(__dirname, 'public')))
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-  
-});
+app.use(express.static(path.join(__dirname, 'public')))
 var XLSX = require('xlsx')
 var workbook = XLSX.readFile(`${__dirname}/test.xlsx`);
 var sheet_name_list = workbook.SheetNames;
